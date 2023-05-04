e3a1e0d66e2e03b8bcefbae298b0cfafdd54e0d1
Lev Goncharov <lev.goncharov@t-systems.com>
2023-05-04 12:42:35 +0200

    fix pip

--- vagrant/install_ansible.sh
+++ vagrant/install_ansible.sh
@@ -2,6 +2,6 @@
 set -eu
 
 sudo dnf install -y python38 python38-pip python38-setuptools
-pip install --upgrade pip
-pip install --upgrade setuptools==62.0.0
-pip install -r /vagrant/vagrant/requirements.txt
+pip3.8 install --upgrade pip
+pip3.8 install --upgrade setuptools==62.0.0
+pip3.8 install -r /vagrant/vagrant/requirements.txt
