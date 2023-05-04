a0f13b42acdf1b4d97083150485a49c3bf1af830
Lev Goncharov <lev.goncharov@t-systems.com>
2023-05-04 13:51:18 +0200

    install ansible for vagrant user

--- vagrant/install_ansible.sh
+++ vagrant/install_ansible.sh
@@ -2,6 +2,6 @@
 set -eu
 
 sudo dnf install -y python38 python38-pip python38-setuptools
-pip3.8 install --upgrade pip
-pip3.8 install --upgrade setuptools==62.0.0
-pip3.8 install -r /vagrant/vagrant/requirements.txt
+sudo -u vagrant pip3.8 install --user --upgrade pip
+sudo -u vagrant pip3.8 install --user --upgrade setuptools==62.0.0
+sudo -u vagrant pip3.8 install --user -r /vagrant/vagrant/requirements.txt
